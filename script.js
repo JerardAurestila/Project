21481640b5cb720a910adcec994e2d99ad0ab423
Jerard_Aurestila <jerardaurestila@gmail.com>
2024-02-26 13:13:34 +0800

    Add 2 Features

---
+++ script.js
@@ -0,0 +1,13 @@
+;function updateDateTime(){
+    var currentDate = new Date();
+    var dateString =
+currentDate.toDateString();
+    var timeString =
+    currentDate.toLocaleTimeString();
+    
+document.getElementById("datetime").innerHTML = "Date: " + dateString + "<br>Time: " + timeString;
+}
+
+
+    setInterval(updateDateTime, 1000);
+    updateDateTime();
